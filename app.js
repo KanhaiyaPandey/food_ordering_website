8c80a33a8b926f8f4267ca04ec9059a6cd001cb6
kanhaiya pandey <kanhaiyapandey2232@mail.com>
2023-09-16 09:35:59 +0530

    installed npm, Parcel devDependency, React, ReactDOM.

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-const heading = React.createElement("h1",{},"namaste everyone ! ");
 
-const heading2 = React.createElement("h2",{},"namaste everyone ! ");
+/**  Doings of parcel
+ * 
+ * Created A Server
+ * HMR - Hot Module Replacement.
+ * File Watcher Algorithm - C++.
+ * Bundling
+ * Minimization
+ * Compression
+ * Cleaning Our Code.
+ * Super fast algorithm.
+ * Media Optimization.
+ * Caching While Development.
+ * Older version browser Compatablity.
+ * 
+ * 
+ * 
+ * 
+ * */ 
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1",{id: "title"},"namaste everyone ! ");
+
+const heading2 = React.createElement("h2",{id: "title"},"namaste everyone ! ");
 
 
 const container = React.createElement("div",{id: "container"},[heading, heading2]);
