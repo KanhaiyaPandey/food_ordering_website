9c3a04a15c33267834d78d080e06bb58335f4eda
kanhaiya pandey <kanhaiyapandey2232@mail.com>
2023-09-18 15:59:48 +0530

    JSX, React Component, Component composistion

--- app.js
+++ app.js
@@ -1,36 +1,33 @@
 
-/**  Doings of parcel
- * 
- * Created A Server
- * HMR - Hot Module Replacement.
- * File Watcher Algorithm - C++.
- * Bundling
- * Minimization
- * Compression
- * Cleaning Our Code.
- * Super fast algorithm.
- * Media Optimization.
- * Caching While Development.
- * Older version browser Compatablity.
- * 
- * 
- * 
- * 
- * */ 
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1",{id: "title"},"namaste everyone ! ");
+//  JSX 
+const Container = () =>( <div id="container">
+              <h1> Namaste React</h1>
+            <h2> Namaste React</h2>
+          </div>
+);
 
-const heading2 = React.createElement("h2",{id: "title"},"namaste everyone ! ");
 
+// React component
+// Functional Component - Functional component is a noraml function whic return JSX or any React element.
+// It is a good practice to start the name of your componenet with capital letter.But its is not mandatory.
 
-const container = React.createElement("div",{id: "container"},[heading, heading2]);
+const HeaderComponent = () => {
+    return (
+         <div>
+         {Container()}
+        <h1> Namaste React</h1>
+        <h2> Namaste React</h2>
+        </div>
+ );
+}
+// you call call a function inside the { container_for_example } or can write any Js code. This is known a component composistion.
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
 //    passing a react element inside the root.
+//    passing a react component insidethe root we use root.render(< Name_of_component />) .
 
-root.render(container);
+root.render(<HeaderComponent />);
